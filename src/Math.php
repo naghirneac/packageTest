4db87335a177c1fa5c63847da9faf7749e558fba
Ana <nag.anya@mail.ru>
2019-11-28 16:39:59 +0200

    Initial commit

---
+++ src/Math.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Math
+{
+    public function ariaPatrat(int $a)
+    {
+
+        $bn = new \Moontoast\Math\BigNumber('9,223,372,036,854,775,808');
+        $bn->multiply(35);
+
+        var_dump($bn->getValue());
+        var_dump($bn->convertToBase(16));
+        return $a*$a;
+    }
+
+}
